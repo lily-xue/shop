28995620443991b05a659ce6896120b463ebfeec
= <=>
2016-07-14 01:32:33 -0700

    添加IsActived字段

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709010621) do
+ActiveRecord::Schema.define(version: 20160714083001) do
 
   create_table "products", force: true do |t|
     t.string   "name"
