538adfef120b4d8cf94bc26fdef5eb7d6bfe9cde
= <=>
2016-07-14 04:52:24 -0700

    解决点击链接wq不能激活的bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,16 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def pro_activate
- user = User.find_by_name(params[:name])
- #if (user != nil && user.IsActived == false && user.ActiveCode ==params[:ActiveCode] )then
- if  user.IsActived == false then
-
-user.update_attribute(IsActived: true)
-flash[:notice] = "激活成功"
-elsif user != nil and user.IsActived == true then
- flash[:notice] = "已经激活了~"
-else 
- flash[:notice] = "激活失败"
-end
-
-end
+	user = User.find_by_name(params[:name])
+	if (user != nil && user.IsActived == "f" && user.ActiveCode ==params[:ActiveCode] )then
+		user.update_attribute(:IsActived, true)
+		flash[:notice] = "激活成功"
+	elsif user != nil and user.IsActived == "t" then
+		flash[:notice] = "你已经激活过了~"
+	else 
+		flash[:notice] = "激活失败"
+	end
+  end
 
 
   def index
